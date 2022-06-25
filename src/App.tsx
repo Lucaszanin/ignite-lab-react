c0242a91ef54f0a44917281b731c628e5327ff6a
unknown <lukinhas.so.lokura@hotmail.com>
2022-06-25 18:58:10 -0300

    APP

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { ApolloProvider } from "@apollo/client"
+import { Event } from "./pages/Event"
+import { Router } from "./Router"
+import { client } from './lib/apollo'
+import { BrowserRouter } from "react-router-dom"
+
+function App() {
+
+  return (
+    <div>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
+    </div>
+  )
+}
+
+export default App
